93f16eddecc971c768a2762e567df2becf7715bc
Ahmad Aburizaiza - Tips for GIS <tipsforgis@gmail.com>
2017-09-24 19:58:53 -0400

    Add files via upload

---
+++ src/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <app-server-status v-for='server in 5'></app-server-status>
+    </div>
+</template>
+<script>
+    import server_status from './Server_status.vue'
+
+    export default{
+        components: {
+            'app-server-status': server_status
+        }
+    }
+</script>
---
+++ src/Server_status.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>Server status: {{status}}</p>
+    <hr />
+    <button v-on:click='change_status'>Change status</button>
+  </div>
+</template>
+<script>
+    export default{
+        data(){
+            return{
+                status: 'Critical'
+            }
+        },
+        methods:{
+            change_status(){
+                this.status = 'Normal';
+            }
+        }
+    }
+</script>
